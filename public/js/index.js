fc39691bd34776e6b6dbfcfc98a6c0bc997c0e4e
jyhorton <jaydenhorton2@gmail.com>
2019-01-29 01:50:19 -0800

    Add event listener to server andd client

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Connected to the server');
+	
+	socket.emit('createMessage', {
+		from: "Andy",
+		text: "That works for me"
+	});
+});
+
+socket.on('disconnect', function () {
+	console.log('Disconnected from server');
+});		
+		
+socket.on('newMessage', function (message) {
+	console.log('newMessage', message);
+});	
+
+	
