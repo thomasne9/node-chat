9f685cacb77846f43b7305035afe1e61ed6633bc
jyhorton <jaydenhorton2@gmail.com>
2019-01-30 01:21:57 -0800

    deploy

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to the server');
 	
-	socket.emit('createMessage', {
+/* 	socket.emit('createMessage', {
 		from: "Andy",
 		text: "That works for me"
-	});
+	}); */
 });
 
 socket.on('disconnect', function () {
