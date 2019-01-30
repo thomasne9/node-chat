74fc1096945cf4511fdfb69171b66bcdbdaf1881
jyhorton <jaydenhorton2@gmail.com>
2019-01-30 01:41:49 -0800

    broadcast

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 	
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+	
 
 	
 	socket.on('createMessage', (message) => {
@@ -23,6 +35,12 @@ io.on('connection', (socket) => {
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+		
+/* 		socket.broadcast.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		}); */
 	});	
 	
 	socket.on('disconnect', () => {
